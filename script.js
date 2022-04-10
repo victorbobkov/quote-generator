9e2a4e6e06ae5fface6f9d029e6961641f92d04a
victor bobkov <victorbobkov@gmail.com>
2022-04-10 05:42:53 +0400

    Fetch data

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let apiQuotes = []
 
+// Show New Quote
+const newQuote = () => {
+   // Pick a random quote from apiQuotes array
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+   console.log(quote)
+}
+
 // Get Quotes from API
 async function getQuotes() {
    const apiUrl = 'https://api.npoint.io/0cd43e45a29f6caa9cba'
@@ -7,7 +14,7 @@ async function getQuotes() {
    try {
       const res = await fetch(apiUrl)
       apiQuotes = await res.json()
-      console.log(apiQuotes)
+      newQuote()
    } catch (e) {
       // Catch Error Here
    }
